b31b2be84f46bc158d8922bda0937fa0d211f611
shawa <shawa1@tcd.ie>
2017-12-04 01:59:52 +0000

    Tidy the turty

--- 03/part2.py
+++ 03/part2.py
@@ -1,9 +1,9 @@
 from turtle import *
-from time import sleep
 from collections import defaultdict
 from itertools import product
+import random
 
-stepsize = 75
+stepsize = 70
 
 mem = defaultdict(int)
 sequence = []
@@ -11,82 +11,84 @@ sequence = []
 FOUND = False
 TARGET = 0
 
-def position():
-    return tuple(p for p in pos())
+STROKE_COLOR = '#ff8888'
+SUPREME = "#d02120"
 
-def offsets(x):
-    offs = -1, 0, 1
-    return tuple([x + (n * stepsize) for n in offs])
+def position():
+    corrected = tuple(int(p) for p in pos())
+    setpos(corrected)
+    return corrected
+
 
 def adjacents(x_y):
+    def offsets(x):
+        return tuple([x + (n * stepsize) for n in (-1, 0, 1)])
     x, y = x_y
     xs = offsets(x)
     ys = offsets(y)
     return tuple(product(xs, ys))
    
 
-def turn(): 
-    left(90)
-
-
 def step():
     global FOUND
-    print(position())
     forward(stepsize)
-    setpos(int(x) for x in position())
+
     pt = position()
-    adjacent_p = adjacents(pt)
-    friends = [mem[p] for p in adjacent_p]
+    friends = [mem[p] for p in adjacents(pt)]
     new = sum(friends)
-    FOUND = new > TARGET
-    print(pt)
-    print(friends)
-    print(adjacent_p)
-    print(new)
-    sequence.append(new)
-    pencolor('#ffffff')
-    if FOUND:
-        pencolor('#ddffdd')
-    write(new, align='center', font=('Futura', 17, 'italic'))
-    pencolor('#ffaaaa')
+
     mem[pt] = new
+    sequence.append(new)
+
+    FOUND = new > TARGET
+
+    pencolor('#000000' if FOUND else '#ffffff')
+    write(new, align='right', font=('Futura', 17, 'italic'))
+    pencolor(STROKE_COLOR)
 
 
-def end():
-    sleep(999999)
-
-
-def spiral(n=100):
+def spiral(target=100):
     global TARGET
     global FOUND
-    TARGET=n
-    mem[pos()] = 1
-    sleep(1)
 
-    def go(i):
+    TARGET = target
+
+    mem[position()] = 1
+
+    i = 0
+    done = False
+    while not done:
+        i = i + 1
         for _ in range(2):
             for _ in range(i):
                 step()
                 if FOUND:
-                    penup()
-                    forward(stepsize)
-                    sleep(0.5)
-                    goto(0,0)
-                    return True
-            turn()
-            sleep(0.05)
+                    return
 
-    i = 1
-    done = False
-    while not done:
-        done = go(i)
-        i = i + 1
-        
+            left(90)
 
-fillcolor('#ffffff')
-shape('classic')
-shapesize(1, 1)
-speed('normal')
-pencolor('#ffaaaa')
-bgcolor("#D02120")
-pendown()
+
+def setup():
+    fillcolor(STROKE_COLOR)
+    shape('classic')
+    shapesize(1, 1)
+    speed('normal')
+    pencolor(STROKE_COLOR)
+    bgcolor(SUPREME)
+    width(2)
+    pendown()
+
+
+def random_walk():
+    while True:
+        random.choice((left, right))(random.randint(1, 80))
+        forward(5)
+
+def main(target=368078):
+    setup()
+    spiral(target)
+    random_walk()
+
+
+if __name__ == '__main__':
+    main()
